3d69243f4f735aefa0574e6adb76bee2383445a6
Viet Nguyen <nguyenhoangviet24022@gmail.com>
2023-07-11 13:54:05 -0500

    Finished 11,12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((prev, current) => {
+        if (!("yearOfDeath" in current)) current.yearOfDeath = (new Date()).getFullYear();
+        if (!("yearOfDeath" in prev)) prev.yearOfDeath = (new Date()).getFullYear();
+        return current.yearOfDeath - current.yearOfBirth > prev.yearOfDeath - prev.yearOfBirth ? current : prev;
+    });
 };
 
 // Do not edit below this line
