1f45cc72ce7d116eda6b937f8b2d699eb316688b
Viet Nguyen <nguyenhoangviet24022@gmail.com>
2023-07-11 12:41:51 -0500

    Finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+// const add = function(...theArgs) {
+// 	let result= 0;
+//   for(let n of theArgs) {
+//     result += +n;
+//   }
+//   return result;
+// };
+const add = function(a,b) {return Number(a)+Number(b)};
+
+// const subtract = function(...theArgs) {
+//   return Math.abs(theArgs[0]-theArgs[1])
+// };
+const subtract = function(a,b) {return Math.abs(Number(a) - Number(b))};
+
+// const sum = function(theArgs) {
+// 	let result = 0;
+//   for(let i = 0; i < theArgs.length; i++) {
+//     result += Number(theArgs[i]);
+//   }
+//   return result;
+// };
+const sum = function(theArgs) {return theArgs.reduce((result, current) => result+current, 0)}
+
+const multiply = function(...theArgs) {
+  let result = 1;
+  for(let n of theArgs) {
+    result *= +n;
+  }
+  return result;
 };
 
-const subtract = function() {
-	
-};
+// const power = function(...theArgs) {
+// 	if (theArgs[1] === 0) return 1;
+//   let result = 1;
+//   for (let i = 0; i < theArgs[1]; i++) {
+//     result *= theArgs[0];
+//   }
+//   return result;
+// };
+const power = function(a,b) {return Math.pow(a,b)}
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1;
+  return number*factorial(number-1);
 };
 
 // Do not edit below this line
