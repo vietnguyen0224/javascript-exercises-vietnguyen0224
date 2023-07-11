f00663e483bce2efd5c8dfbd7e78af381cbe36a2
Viet Nguyen <nguyenhoangviet24022@gmail.com>
2023-07-11 13:17:57 -0500

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    let result = [1,1];
+    for (let i = 2; i < index; i++) {
+        result.push(result[i-1]+result[i-2])
+    }
+    return result[index-1]
 };
 
 // Do not edit below this line
