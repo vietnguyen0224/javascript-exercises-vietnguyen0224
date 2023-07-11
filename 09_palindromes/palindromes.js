76dc5a42ed67f311e73bf8aaddee741804ea3c30
Viet Nguyen <nguyenhoangviet24022@gmail.com>
2023-07-11 13:08:45 -0500

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let nonAlphaNumeric = /[^A-Za-z0-9]/g;
+    let arr = str.toLowerCase().replace(nonAlphaNumeric,'').split('');
+    let oldArr = [...arr];
+    return arr.reverse().join() === oldArr.join();
 };
 
 // Do not edit below this line
